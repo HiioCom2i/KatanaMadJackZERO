6167cccb6a736a20ceb97077a0e25144eea6056f
Davi Barbosa Gibson <davigibson10@gmail.com>
2025-07-20 17:28:12 -0300

    som da katana e ataque funcional na cena principal

--- Assets/Project/Scripts/PlayerScripts/PlayerAttack.cs
+++ Assets/Project/Scripts/PlayerScripts/PlayerAttack.cs
@@ -1,3 +1,5 @@
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
@@ -7,19 +9,32 @@ public class PlayerAttack : MonoBehaviour
     public int attackDamage = 1;
     public Transform attackPoint;
     public LayerMask enemyLayers;
-    public Animator katanaAnimator;
+    //public Animator katanaAnimator;  COOLDOWN N FUNCIONA SEM A ANIMAÇÃO
 
 
     [Header("Cooldown")]
     public float attackRate = 2f; // ataques por segundo
     private float nextAttackTime = 0f;
 
+    // VARIÁVEIS FMOD
+    private EventInstance ataque;
+    private EventInstance ataque_no_inimigo;
+
+    void Start()
+    {
+        ataque = RuntimeManager.CreateInstance("event:/Player_Ataca_Espada");
+        ataque_no_inimigo = RuntimeManager.CreateInstance("event:/Player_AtacaAtinge_Espada");
+    }
+
     void Update()
     {
-        if (Time.time >= nextAttackTime && Input.GetButtonDown("Fire1"))
+       if (Time.timeScale == 0) return;
+
+        if (Time.unscaledTime >= nextAttackTime && Input.GetKeyDown(KeyCode.Mouse0))
         {
+            Debug.Log("Ataque com cooldown respeitado");
             Attack();
-            nextAttackTime = Time.time + 1f / attackRate;
+            nextAttackTime = Time.unscaledTime + 1f / attackRate;
         }
     }
 
@@ -30,17 +45,29 @@ public class PlayerAttack : MonoBehaviour
         // Detectar inimigos no alcance
         Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
 
+        bool atingiuAlvo = false;
+
         foreach (Collider enemy in hitEnemies)
         {
             Debug.Log("Acertou " + enemy.name);
-            // Aqui você pode acessar o script do inimigo e causar dano, tipo:
             enemy.GetComponentInParent<JoaoCarlos>()?.TakeDamage(attackDamage);
-
-
+            atingiuAlvo = true;
         }
 
-        katanaAnimator.SetTrigger("Attack");
+
+        if (atingiuAlvo)
+        {
+            ataque_no_inimigo.start(); // Se atingiu algum inimigo
+        }
+        else
+        {
+            ataque.start(); // Se não atingiu ninguém
+        }
+
+        // Animação
+        //katanaAnimator.SetTrigger("Attack");
     }
+
 
     // Gizmo pra visualizar o alcance do ataque no editor
     void OnDrawGizmosSelected()
@@ -51,4 +78,14 @@ public class PlayerAttack : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
+    
+    void OnDestroy()
+    {
+        ataque.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        ataque.release();
+        
+        ataque_no_inimigo.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        ataque_no_inimigo.release();
+    }
+
 }
